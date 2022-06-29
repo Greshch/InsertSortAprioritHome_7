bc324ef70dd8f7622528db3b8be86860999f6865
Greshch <greshchook@gmail.com>
2022-06-29 05:58:15 +0300

    it was finished

--- InsertSortAprioritHome_7.cpp
+++ InsertSortAprioritHome_7.cpp
@@ -3,26 +3,58 @@
 
 #include <iostream>
 
-void ArrayPrint(int* b, int* e);
-void InsertSort(int* arr, int sz);
 
 int main()
 {
-    int arr[] { 1, 9, 8, 6, 7, 2, 4, 5, 3 };
+    const int arr[] { 1, 9, 8, 6, 7, 2, 4, 5, 3 };
+    const int* ptrArr[10] = {};
+
     const int szArr = sizeof(arr) / sizeof(arr[0]);
-    ArrayPrint(arr, arr + szArr);
-    InsertSort(arr, szArr);
-    ArrayPrint(arr, arr + szArr);
+    const int* e = arr + szArr;
+    // fill array
+    for (int i = 0; i < szArr; i++)
+    {
+        ptrArr[i] = arr + i;
+    }
+
+    // print array
+    for (int i = 0; i < szArr; i++)
+    {
+        std::cout << *(ptrArr[i]) << " ";
+    } std::cout << "\n";
+
+    //InsertSort
+    ptrArr[0] = arr;
+    for (int i = 1; i < szArr; i++)
+    {
+        int j = i;
+        //int cur = arr[i];
+        //int cur = *(ptrArr[i]);
+        const int* curPtr = ptrArr[i];
+        while (j >= 0 && *curPtr <= /*arr[j]*/ *(ptrArr[j]))
+        {
+            --j;
+        }
+        ++j;
+        
+        for (int k = i; k >= j; k--)
+        {
+            //arr[k] = arr[k - 1];
+            ptrArr[k] = ptrArr[k - 1];
+        }
+        //arr[j] = cur;
+        ptrArr[j] = curPtr;
+    }
+
+    //print array
+    for (int i = 0; i < szArr; i++)
+    {
+        std::cout << *(ptrArr[i]) << " ";
+    } std::cout << "\n";
+    
 }
 
-void ArrayPrint(int* b, int* e)
-{
-    for (; b != e; ++b)
-    {
-        std::cout << *b << " ";
-    }
-    std::cout << "\n";
-}
+
 
 void InsertSort(int* arr, int sz)
 {
