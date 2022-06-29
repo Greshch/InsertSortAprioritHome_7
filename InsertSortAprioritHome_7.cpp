756c571974f2d28a12eaeb17f424d702f3a98362
Greshch <greshchook@gmail.com>
2022-06-29 04:45:54 +0300

    non const int* was implemented

--- InsertSortAprioritHome_7.cpp
+++ InsertSortAprioritHome_7.cpp
@@ -3,8 +3,44 @@
 
 #include <iostream>
 
+void ArrayPrint(int* b, int* e);
+void InsertSort(int* arr, int sz);
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    int arr[] { 1, 9, 8, 6, 7, 2, 4, 5, 3 };
+    const int szArr = sizeof(arr) / sizeof(arr[0]);
+    ArrayPrint(arr, arr + szArr);
+    InsertSort(arr, szArr);
+    ArrayPrint(arr, arr + szArr);
 }
 
+void ArrayPrint(int* b, int* e)
+{
+    for (; b != e; ++b)
+    {
+        std::cout << *b << " ";
+    }
+    std::cout << "\n";
+}
+
+void InsertSort(int* arr, int sz)
+{
+    for (int i = 1; i < sz; i++)
+    {
+        int j = i;
+        int cur = arr[i];
+        while (j >= 0 && cur <= arr[j])
+        {
+            --j;
+        }
+        ++j;
+        //std::cout << "j = " << j << "\n";
+        for (int k = i; k >= j; k--)
+        {
+            arr[k] = arr[k - 1];
+        }
+        arr[j] = cur;
+    }
+    
+}
